e80c6bdce6a4cd7d635154bb91a1c3d82c95a76a
DJ Alex <DJ Alex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-20 08:01:29 +0000

    tagging version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/intelliads/tags/2.1@700631 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- intelliads.php
+++ intelliads.php
@@ -4,12 +4,12 @@
 
 /* 
 
-	Plugin Name: IntelliAds
-	Plugin URI: http://ppc.intellichristian.com
+	Plugin Name: IntelliChristian! Ads, Affiliate Program, and Link Exchange
+	Plugin URI: http://wordpress.org/intelliads
 	Description: Insert IntelliChristian! Ads into various places in your blog posts
 	Author: DJ Alex
 	Version: 1.2
-	Author URI: http://ppc.intellichristian.com
+	Author URI: http://djalexplugins.weebly.com
 
 */
 
@@ -33,47 +33,116 @@
 
 */
 
-global $QData;
-$QData['AdsWid'] = 3; /* Ads on Widget */
-$QData['Ads'] = 3; /* Ads on Post body */
-$QData['Name'] = 'IntelliAds';
-$QData['Version'] = '1.2';
-$QData['URI'] = 'http://ppc.intellichristian.com';
-$QData['AdsWidName'] = 'AdsWidget%d (IntelliAds)';
-$QData['Default'] = array(
 
-	'AdsDisp'=>'3',
-	'BegnAds'=>true,'BegnRnd'=>'1','EndiAds'=>true,'EndiRnd'=>'0','MiddAds'=>false,'MiddRnd'=>'0','MoreAds'=>false,'MoreRnd'=>'0',
-	'Par1Ads'=>false,'Par1Rnd'=>'0','Par1Nup'=>'0','Par1Con'=>false,
-	'Par2Ads'=>false,'Par2Rnd'=>'0','Par2Nup'=>'0','Par2Con'=>false,
-	'Par3Ads'=>false,'Par3Rnd'=>'0','Par3Nup'=>'0','Par3Con'=>false,
-	'Img1Ads'=>false,'Img1Rnd'=>'0','Img1Nup'=>'0',
-	'AppPost'=>true,'AppPage'=>true,'AppHome'=>false,'AppCate'=>false,'AppArch'=>false,'AppTags'=>false,'AppMaxA'=>false,'AppSide'=>false,'AppLogg'=>false,
-	'QckTags'=>true,'QckRnds'=>false,'QckOffs'=>false
+add_action("widgets_init", array('IntelliChristian_Links', 'register'));
+register_activation_hook( __FILE__, array('IntelliChristian_Links', 'activate'));
+register_deactivation_hook( __FILE__, array('IntelliChristian_Links', 'deactivate'));
+class IntelliChristian_Links {
+  function activate(){
+    $data = array( 'option1' => 'Default value' ,'option2' => 55);
+    if ( ! get_option('widget_name')){
+      add_option('widget_name' , $data);
+    } else {
+      update_option('widget_name' , $data);
+    }
+  }
+  function deactivate(){
+    delete_option('widget_name');
+  }
+  function control(){
+    echo '<a href="http://ppc.intellichristian.com/link-exchange">Submit an ad about your site here</a>';
+  }
+  function widget($args){
+    echo $args['before_widget'];
+    echo $args['before_title'] . 'Links' . $args['after_title'];?>
+<script src="http://ppc.intellichristian.com/ads/ads.php?t=MTA0MjsyMztzcXVhcmUuc21hbGxfcmVjdGFuZ2xl"></script>
+<?php
+  echo $args['after_widget'];
+  }
+  function register(){
+    register_sidebar_widget('IntelliChristian Link Exchange', array('IntelliChristian_Links', 'widget'));
+    register_widget_control('IntelliChristian Link Exchange', array('IntelliChristian_Links', 'control'));
+  }
+}
 
-); 
+add_action("widgets_init", array('IntelliChristian_Affiliate', 'register'));
+register_activation_hook( __FILE__, array('IntelliChristian_Affiliate', 'activate'));
+register_deactivation_hook( __FILE__, array('IntelliChristian_Affiliate', 'deactivate'));
+class IntelliChristian_Affiliate {
+  function activate(){
+    $data = array( 'option1' => 'Default value' ,'option2' => 55);
+    if ( ! get_option('widget_name')){
+      add_option('widget_name' , $data);
+    } else {
+      update_option('widget_name' , $data);
+    }
+  }
+  function deactivate(){
+    delete_option('widget_name');
+  }
+  function control(){
+    echo 'This widget will add all the code you need to display products from IntelliChristian! Affiliate Program, but you will need to sign up at <a href="http://intellichristian.com/affiliateprogram">http://intellichristian.com/affiliateprogram</a> for payments and statistics.';
+  }
+  function widget($args){
+    echo $args['before_widget'];
+    echo $args['before_title'] . 'Affiliate Products' . $args['after_title'];?>
+<script src="http://ppc.intellichristian.com/ads/ads.php?t=MTA2NDsyNzt2ZXJ0aWNhbC5iYW5uZXI="></script>
+<?php
+  echo $args['after_widget'];
+  }
+  function register(){
+    register_sidebar_widget('IntelliChristian Affiliate Program', array('IntelliChristian_Affiliate', 'widget'));
+    register_widget_control('IntelliChristian Affiliate Program', array('IntelliChristian_Affiliate', 'control'));
+  }
+}
 
-$QData['DefaultAdsOpt'] = array(
+add_action("widgets_init", array('IntelliChristian_Ads', 'register'));
+register_activation_hook( __FILE__, array('IntelliChristian_Ads', 'activate'));
+register_deactivation_hook( __FILE__, array('IntelliChristian_Ads', 'deactivate'));
+class IntelliChristian_Ads {
+  function activate(){
+    $data = array( 'option1' => 'Default value' ,'option2' => 55);
+    if ( ! get_option('widget_name')){
+      add_option('widget_name' , $data);
+    } else {
+      update_option('widget_name' , $data);
+    }
+  }
+  function deactivate(){
+    delete_option('widget_name');
+  }
+  function control(){
+    echo '<p>This widget will add all the code you need to display an invitation to advertise on this site. But you will need to sign up at <a href="http://ppc.intellichristian.com/publishers">http://ppc.intellichristian.com/publishers</a>for payments and statistics</p>';
+  }
+  function widget($args){
+    echo $args['before_widget'];
+    echo $args['before_title'] . '' . $args['after_title'];?>
+<!-- IntelliChristian! PayPerClick Ad Code - Targeted Publisher 2 - Small Rectangle (180 x 150) -->
+<script language="javascript" type="text/javascript">
+//Number of ads to offset the result
+offset_25 = 0;
+//Request ads for a specific keyword
+keyword_25 = '<?php echo $_SERVER["SERVER_NAME"]; ?>';
+document.write("<script language='javascript' type='text/javascript' src='http://ppc.intellichristian.com/ads/ads.php?t=MTAwMjsyNTtzcXVhcmUuc21hbGxfcmVjdGFuZ2xl&o=" + offset_25 + "&k=" + escape(keyword_25)  + "'><\/sc" + "ript>");
+</script>
+<!-- End IntelliChristian! PayPerClick Ad Code -->
+<?php
+  echo $args['after_widget'];
+  }
+  function register(){
+    register_sidebar_widget('IntelliChristian Ads', array('IntelliChristian_Ads', 'widget'));
+    register_widget_control('IntelliChristian Ads', array('IntelliChristian_Ads', 'control'));
+  }
+}
 
-	'AdsMargin'=>'10','AdsAlign'=>'2'
 
-);	
+	
 
-$QData['DefaultAdsName'] = array();
 
-for ($i=1;$i<=$QData['Ads'];$i++) { 
 
-	array_push($QData['DefaultAdsName'], 'AdsCode'.$i );
-	array_push($QData['DefaultAdsName'], 'AdsAlign'.$i );
-	array_push($QData['DefaultAdsName'], 'AdsMargin'.$i );
 
-};
 
-for ($i=1;$i<=$QData['AdsWid'];$i++) { 
-
-	array_push($QData['DefaultAdsName'], 'WidCode'.$i );	
-
-};	
+	
 
 
 
@@ -241,581 +310,6 @@ if ($wpvcomp) {
 }
 
 
-
-
-
-$ShownAds = 0;
-$AdsId = array();
-$beginend = 0;
-
-
-
-function process_content($content)
-
-{
-
-	global $QData;
-	global $ShownAds;
-	global $AdsId;
-	global $beginend;
-
-	
-
-	/* verifying */ 
-
-	if(	(is_feed()) ||
-
-			(strpos($content,'<!--NoAds-->')) ||
-			(strpos($content,'<!--OffAds-->')) ||
-			(is_single() && !(get_option('AppPost'))) ||
-			(is_page() && !(get_option('AppPage'))) ||
-			(is_home() && !(get_option('AppHome'))) ||			
-			(is_category() && !(get_option('AppCate'))) ||
-			(is_archive() && !(get_option('AppArch'))) ||
-			(is_tag() && !(get_option('AppTags'))) ||
-			(is_user_logged_in() && (get_option('AppLogg'))) ) { 
-
-		$content = clean_tags($content); return $content; 
-
-	}
-
-	
-
-	$AdsToShow = get_option('AdsDisp');
-
-	for($i=1;$i<=$QData['AdsWid'];$i++) {
-
-		$wadsid = sanitize_title(sprintf($QData['AdsWidName'],$i));
-		$AdsToShow -= (is_active_widget(true, $wadsid)) ? 1 : 0 ;
-
-	}		
-
-	if( $ShownAds >= $AdsToShow ) { $content = clean_tags($content); return $content; };
-
-	if( !count($AdsId) ) {  
-
-		for($i=1;$i<=$QData['Ads'];$i++) { 
-
-			$tmp = trim(get_option('AdsCode'.$i));
-
-			if( !empty($tmp) ) {
-
-				array_push($AdsId, $i);
-
-			}
-
-		}
-
-	}	
-
-	if( !count($AdsId) ) { $content = clean_tags($content); return $content; };
-
-
-
-	/* ... Tidy up content ... */
-
-	$offdef = strpos($content,'<!--OffDef-->');
-
-	if( !$offdef ) {
-
-		$AdsIdCus = array();
-		$cusads = 'CusAds'; $cusrnd = 'CusRnd';
-		$more1 = get_option('MoreAds'); $more2 = get_option('MoreRnd');		
-		$begn1 = get_option('BegnAds'); $begn2 = get_option('BegnRnd');
-		$midd1 = get_option('MiddAds'); $midd2 = get_option('MiddRnd');
-		$endi1 = get_option('EndiAds');	$endi2 = get_option('EndiRnd');
-		$rc=3;
-
-		for($i=1;$i<=$rc;$i++) { 
-
-			$para1[$i] = get_option('Par'.$i.'Ads');	$para2[$i] = get_option('Par'.$i.'Rnd');	$para3[$i] = get_option('Par'.$i.'Nup');	$para4[$i] = get_option('Par'.$i.'Con');
-
-		}
-
-		$imge1 = get_option('Img1Ads');	$imge2 = get_option('Img1Rnd');	$imge3 = get_option('Img1Nup');		
-
-		if ( $begn2 == 0 ) { $b1 = $cusrnd; } else { $b1 = $cusads.$begn2; array_push($AdsIdCus, $begn2); };
-		if ( $more2 == 0 ) { $r1 = $cusrnd; } else { $r1 = $cusads.$more2; array_push($AdsIdCus, $more2); };		
-		if ( $midd2 == 0 ) { $m1 = $cusrnd; } else { $m1 = $cusads.$midd2; array_push($AdsIdCus, $midd2); };
-		if ( $endi2 == 0 ) { $b2 = $cusrnd; } else { $b2 = $cusads.$endi2; array_push($AdsIdCus, $endi2); };	
-
-		for($i=1;$i<=$rc;$i++) { 
-
-			if ( $para2[$i] == 0 ) { $b3[$i] = $cusrnd; } else { $b3[$i] = $cusads.$para2[$i]; array_push($AdsIdCus, $para2[$i]); };	
-
-		}	
-
-		if ( $imge2 == 0 ) { $b4 = $cusrnd; } else { $b4 = $cusads.$imge2; array_push($AdsIdCus, $imge2); };	
-
-		if( $midd1 ) {
-
-			if( substr_count(strtolower($content), '</p>')>=2 ) {
-
-				$sch = "</p>";
-				$content = str_replace("</P>", $sch, $content);
-				$arr = explode($sch, $content);			
-				$nn = 0; $mm = strlen($content)/2;
-				for($i=0;$i<count($arr);$i++) {
-
-					$nn += strlen($arr[$i]) + 4;
-
-					if($nn>$mm) {
-
-						if( ($mm - ($nn - strlen($arr[$i]))) > ($nn - $mm) && $i+1<count($arr) ) {
-
-							$arr[$i+1] = '<!--'.$m1.'-->'.$arr[$i+1];							
-
-						} else {
-
-							$arr[$i] = '<!--'.$m1.'-->'.$arr[$i];
-
-						}
-
-						break;
-
-					}
-
-				}
-
-				$content = implode($sch, $arr);
-
-			}	
-
-		}
-
-		if( $more1 ) {
-
-			$mmr = '<!--'.$r1.'-->';
-			$postid = get_the_ID();
-			$content = str_replace('<span id="more-'.$postid.'"></span>', $mmr, $content);		
-
-		}		
-
-		if( $begn1 ) {
-
-			$content = '<!--'.$b1.'-->'.$content;
-
-		}
-
-		if( $endi1 ) {
-
-			$content = $content.'<!--'.$b2.'-->';
-
-		}
-
-		for($i=$rc;$i>=1;$i--) { 
-
-			if ( $para1[$i] ){
-
-				$sch = "</p>";
-				$content = str_replace("</P>", $sch, $content);
-				$arr = explode($sch, $content);
-
-				if ( (int)$para3[$i] < count($arr) ) {
-
-					$content = implode($sch, array_slice($arr, 0, $para3[$i])).$sch .'<!--'.$b3[$i].'-->'. implode($sch, array_slice($arr, $para3[$i]));
-
-				}	elseif ($para4[$i]) {
-
-					$content = implode($sch, $arr).'<!--'.$b3[$i].'-->';
-
-				}
-
-			}
-
-		}	
-
-		if ( $imge1 ){
-
-			$sch = "<img";
-			$bch = ">";
-			$content = str_replace("<IMG", $sch, $content);
-			$arr = explode($sch, $content);
-
-			if ( (int)$imge3 < count($arr) ) {
-
-				$trr = explode($bch, $arr[$imge3]);
-
-				if ( count($trr) ) {
-
-					$arr[$imge3] = implode($bch, array_slice($trr, 0, 1)).$bch. "\n".'<!--'.$b4.'-->'."\n". implode($bch, array_slice($trr, 1));
-
-				}
-
-				$content = implode($sch, $arr);
-
-			}	
-
-		}		
-
-	}
-
-	
-
-	/* ... Tidy up content ... */
-
-	$content = '<!--EmptyClear-->'.$content."\n".'<div style="font-size:0px;height:0px;line-height:0px;margin:0;padding:0;clear:both"></div>';
-
-	$content = clean_tags($content, true);	
-
-	$ismany = (!is_single() && !is_page());
-
-	$showall = get_option('AppMaxA');
-
-	
-
-	/* ... Replace Beginning/Middle/End Ads1-10 ... */
-
-	if( !$offdef ) {
-
-		for( $i=1; $i<=count($AdsIdCus); $i++ ) {
-
-			if( $showall || !$ismany || $beginend != $i ) {
-
-				if( strpos($content, '<!--'.$cusads.$AdsIdCus[$i-1].'-->') && in_array($AdsIdCus[$i-1], $AdsId)) {
-
-					$content = replace_ads( $content, $cusads.$AdsIdCus[$i-1], $AdsIdCus[$i-1] ); $AdsId = del_element($AdsId, array_search($AdsIdCus[$i-1], $AdsId)) ;
-
-					$ShownAds += 1; if( $ShownAds >= $AdsToShow || !count($AdsId) ){ $content = clean_tags($content); return $content; };
-
-					$beginend = $i; if(!$showall && $ismany){break;} 
-
-				}
-
-			}	
-
-		}	
-
-	}
-
-	
-
-	/* ... Replace Ads1 to Ads10 ... */
-
-	if( $showall || !$ismany ) {
-
-		$tcn = count($AdsId); $tt = 0;
-
-		for( $i=1; $i<=$tcn; $i++ ) {
-
-			if( strpos($content, '<!--Ads'.$AdsId[$tt].'-->') ) {
-
-				$content = replace_ads( $content, 'Ads'.$AdsId[$tt], $AdsId[$tt] ); $AdsId = del_element($AdsId, $tt) ;
-
-				$ShownAds += 1; if( $ShownAds >= $AdsToShow || !count($AdsId) ){ $content = clean_tags($content); return $content; };
-
-			} else {
-
-				$tt += 1;
-
-			}
-
-		}	
-
-	}	
-
-
-
-	/* ... Replace Beginning/Middle/End random Ads ... */
-
-	if( strpos($content, '<!--'.$cusrnd.'-->') && ($showall || !$ismany) ) {
-
-		$tcx = count($AdsId);
-
-		$tcy = substr_count($content, '<!--'.$cusrnd.'-->');
-
-		for( $i=$tcx; $i<=$tcy-1; $i++ ) {
-
-			array_push($AdsId, -1);
-
-		}
-
-		shuffle($AdsId);
-
-		for( $i=1; $i<=$tcy; $i++ ) {
-
-			$content = replace_ads( $content, $cusrnd, $AdsId[0] ); $AdsId = del_element($AdsId, 0) ;
-
-			$ShownAds += 1; if( $ShownAds >= $AdsToShow || !count($AdsId) ){ $content = clean_tags($content); return $content; };
-
-		}
-
-	}
-
-	
-
-	/* ... Replace RndAds ... */
-
-	if( strpos($content, '<!--RndAds-->') && ($showall || !$ismany) ) {
-
-		$AdsIdTmp = array();
-
-		shuffle($AdsId);
-
-		for( $i=1; $i<=$AdsToShow-$ShownAds; $i++ ) {
-
-			if( $i <= count($AdsId) ) {
-
-				array_push($AdsIdTmp, $AdsId[$i-1]);
-
-			}
-
-		}
-
-		$tcx = count($AdsIdTmp);
-
-		$tcy = substr_count($content, '<!--RndAds-->');
-
- 		for( $i=$tcx; $i<=$tcy-1; $i++ ) {
-
-			array_push($AdsIdTmp, -1);
-
-		}
-
-		shuffle($AdsIdTmp);
-
-		for( $i=1; $i<=$tcy; $i++ ) {
-
-			$tmp = $AdsIdTmp[0];
-
-			$content = replace_ads( $content, 'RndAds', $AdsIdTmp[0] ); $AdsIdTmp = del_element($AdsIdTmp, 0) ;
-
-			if($tmp != -1){$ShownAds += 1;}; if( $ShownAds >= $AdsToShow || !count($AdsIdTmp) ){ $content = clean_tags($content); return $content; };
-
-		}
-
-	}	
-
-	
-
-	/* ... That's it. DONE :) ... */
-
-	$content = clean_tags($content); return $content;
-
-}
-
-function clean_tags($content, $trimonly = false) {
-
-	global $QData;
-
-	global $ShownAds;
-
-	global $AdsId;
-
-	global $beginend;
-
-	$tagnames = array('EmptyClear','RndAds','NoAds','OffAds','CusRnd');
-
-	for($i=1;$i<=$QData['Ads'];$i++) { array_push($tagnames, 'CusAds'.$i); array_push($tagnames, 'Ads'.$i); };
-
-	foreach ($tagnames as $tgn) {
-
-		if(strpos($content,'<!--'.$tgn.'-->') || $tgn=='EmptyClear') {
-
-			if($trimonly) {
-
-				$content = str_replace('<p><!--'.$tgn.'--></p>', '<!--'.$tgn.'-->', $content);	
-
-			}else{
-
-				$content = str_replace(array('<p><!--'.$tgn.'--></p>','<!--'.$tgn.'-->'), '', $content);	
-
-			}
-
-		}
-
-	}
-
-	if(!$trimonly && (is_single() || is_page()) ) {
-
-		$ShownAds = 0;
-
-		$AdsId = array();
-
-		$beginend = 0;
-
-	}	
-
-	return $content;
-
-}
-
-function replace_ads($content, $nme, $adn) {
-
-	if( !strpos($content,'<!--'.$nme.'-->') ) { return $content; }	
-
-	global $QData;
-
-	if ($adn != -1) {
-
-		$arr = array('',
-
-			'float:left;margin:%1$dpx %1$dpx %1$dpx 0;',
-
-			'float:none;margin:%1$dpx 0 %1$dpx 0;text-align:center;',
-
-			'float:right;margin:%1$dpx 0 %1$dpx %1$dpx;',
-
-			'float:none;margin:0px;');
-
-		$adsalign = get_option('AdsAlign'.$adn);
-
-		$adsmargin = get_option('AdsMargin'.$adn);
-
-		$style = sprintf($arr[(int)$adsalign], $adsmargin);
-
-		$adscode = get_option('AdsCode'.$adn);
-
-		$adscode =
-
-			"\n".'<!-- '.$QData['Name'].' Wordpress Plugin: '.$QData['URI'].' -->'."\n".
-
-			'<div style="'.$style.'">'."\n".
-
-			$adscode."\n".
-
-			'</div>'."\n";
-
-	} else {
-
-		$adscode ='';
-
-	}	
-
-	$cont = explode('<!--'.$nme.'-->', $content, 2);	
-
-	return $cont[0].$adscode.$cont[1];
-
-}
-
-function del_element($array, $idx) {
-
-  $copy = array();
-
-	for( $i=0; $i<count($array) ;$i++) {
-
-		if ( $idx != $i ) {
-
-			array_push($copy, $array[$i]);
-
-		}
-
-	}	
-
-  return $copy;
-
-}
-
-add_filter('the_content', 'process_content');
-
-
-
-
-
-function ads_widget_register() {
-
-	global $QData;
-
-	if (!function_exists('register_sidebar_widget') || !function_exists('register_widget_control')) { return; };
-
-  for($i=1;$i<=$QData['AdsWid'];$i++) {
-
-		if(get_option('WidCode'.$i)!='') {
-
-			$displaystr =
-
-				'$cont = get_the_content("",0,"");'.
-
-				'if( !strpos($cont,"<!--OffAds-->") && !(is_home()&&get_option("AppSide")) ) {'.
-
-				'extract($args);'.
-
-				'$title = get_option("WidCode-title-'.$i.'");'.
-
-				'$codetxt = get_option("WidCode'.$i.'");'.
-
-				'echo "\n"."<!-- Quick Adsense Wordpress Plugin: http://quicksense.net/ -->"."\n";'.
-
-				'echo $before_widget."\n";'.
-
-				'if (!empty($title)) { '.
-
-				'echo $before_title.$title.$after_title."\n"; '.
-
-				'};'.
-
-				'echo $codetxt;'.
-
-				'echo "\n".$after_widget;'.
-
-				'}';
-
-			$controlstr	=
-
-				'if ($_POST["WidCodeSubmit'.$i.'"]) {'.
-
-				'  update_option_en("WidCode-title-'.$i.'", $_POST["WidCode-title-'.$i.'"], "strip_tags");'.
-
-				'  update_option_en("WidCode'.$i.'", $_POST["WidCode'.$i.'"]);'.
-
-				'}'.
-
-				'$title = get_option_en("WidCode-title-'.$i.'");'.
-
-				'$codetxt = get_option_en("WidCode'.$i.'");'.
-
-				'echo "<p><label>'.__('Title:').'</label>";'.
-
-				'echo "<input style=\"width:100%;\" id=\"WidCode-title-'.$i.'\" name=\"WidCode-title-'.$i.'\" type=\"text\" value=\"".$title."\" />";'.
-
-				'echo "<p><textarea style=\"width:100%;height:150px;\" id=\"WidCode'.$i.'\" name=\"WidCode'.$i.'\">".$codetxt."</textarea><br\>";'.
-
-				'echo "<a style=\"font-size:8pt;text-decoration:none\" href=\"options-general.php?page='.basename(__FILE__).'\">'.__('Quick Adsense Setting').'</a></p>";'.
-
-				'echo "<input type=\"hidden\" id=\"WidCodeSubmit'.$i.'\" name=\"WidCodeSubmit'.$i.'\" value=\"true\" />";';
-
-			$displaycall[$i] = create_function('$args', $displaystr);
-
-			$controlcall[$i] = create_function('', $controlstr);
-
-			$wadsid = sprintf($QData['AdsWidName'],$i);
-
-			register_sidebar_widget($wadsid, $displaycall[$i], "adswidget");
-
-			register_widget_control($wadsid, $controlcall[$i]);
-
-		}			
-
-	}
-
-}	
-
-function get_option_en($nameid)
-
-{
-
-	$txt = get_option($nameid);
-
-	$txt = htmlspecialchars($txt, ENT_QUOTES);
-
-	if(!empty($txt)) { return $txt; }else{ return ""; };
-
-}
-
-function update_option_en($nameid, $text, $opt='')
-
-{
-
-	$txt = stripslashes($text);
-
-	if ($opt=='strip_tags') { $txt = strip_tags($txt); };
-
-	update_option($nameid, $txt);
-
-	if(!empty($txt)) { return $txt; }else{ return ""; };
-
-}
-
-add_action('init', 'ads_widget_register');
-
 ?>
+
+
